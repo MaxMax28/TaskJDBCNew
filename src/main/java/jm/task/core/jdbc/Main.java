64029e3448af353067805de12d43c4b3d43401cc
MaxVer <28.max.05.max.91@gmail.com>
2020-06-24 16:04:45 +0600

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+        UserServiceImpl uSImpl = new UserServiceImpl();
+        uSImpl.createUsersTable();
+        uSImpl.saveUser("Ivan", "Ivanov", (byte) 15);
+        uSImpl.saveUser("Petr", "Petrov", (byte) 20);
+        uSImpl.saveUser("Vasily", "Vasechkin", (byte) 25);
+        uSImpl.saveUser("Feodor", "Feodorov", (byte) 30);
+        System.out.println(uSImpl.getAllUsers().toString());
+        uSImpl.createUsersTable();
+        uSImpl.dropUsersTable();
     }
 }
